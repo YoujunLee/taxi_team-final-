13daf973aec27e3bd26e61fdf5d0b9f902bd1bd2
YoujunLee <injxyj@gmail.com>
2015-07-03 17:08:54 +0900

    aptana git에 연결

---
+++ practice_ymg/SignUp.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		 <meta charset="utf-8">
+   		 <title>회원가입</title>
+   		 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+   		 <link rel="stylesheet" type="text/css" href="../css/box.css">
+	</head>
+	<body>
+		<div class="div_root">
+		<br><br><br><br>
+		<div class="jumbotron">
+  			<h1>회원가입</h1>
+  		</div>
+  		<br><br><br>
+  		<?php
+  		$no;?>
+		<form align="center" class="form-horizontal" action="./mysql_practice.php" method=post>
+      <input type = hidden name = no value= <?=$no?>>
+		    <div class="form-group">
+		      <label for="inputName" class="col-lg-2 control-label">이름</label>
+		      <div class="col-lg-10">
+		        <input type=text class="form-control" id="inputName" placeholder="Name" name = name>
+		      </div>
+		    </div>
+		    <div class="form-group">
+		      <label for="inputStudentId" class="col-lg-2 control-label">학번</label>
+		      <div class="col-lg-10">
+		        <input type="text" class="form-control" id="inputStudentId" placeholder="Student ID">
+		      </div>
+		    </div>
+		    <br><br>
+		    <div class="form-group">
+		      <label for="inputId" class="col-lg-2 control-label">아이디</label>
+		      <div class="col-lg-10">
+		        <input type="text" class="form-control" id="inputId" placeholder="Id">
+		      </div>
+		    </div>
+			<div class="form-group">
+		      <label for="inputPassword" class="col-lg-2 control-label">비밀번호</label>
+		      <div class="col-lg-10">
+		        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+		      </div>
+		    </div>
+		    <div class="form-group">
+		      <label for="inputPasswordTwice" class="col-lg-2 control-label">비밀번호 확인</label>
+		      <div class="col-lg-10">
+		        <input type="password" class="form-control" id="inputPasswordTwice" placeholder="Password">
+		      </div>
+		    </div>
+		    <br>
+		   
+		     <div class="div_go">
+                <input class="btn btn-lg btn-block" type="submit" value="가입하기">
+			</div>
+
+		</form>
+		</div>
+	</body>
+</html>
