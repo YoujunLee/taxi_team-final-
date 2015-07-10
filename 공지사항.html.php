b73ae3d6cc2a625d05995f64515199d9d90ff825
정마리아 <jmary625@google.com>
2015-07-10 14:41:40 +0900

    java script 추가

--- 공지사항.html.php
+++ 공지사항.html.php
@@ -2,10 +2,32 @@
 
 <html>
 	<head>
-		 <meta charset="utf-8">
-		 <title>공지사항</title>
-		 <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
-		 <link rel="stylesheet" type="text/css" href="./css/mypage.css">
+		<meta charset="utf-8">
+		<title>공지사항</title>
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="./css/mypage.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script> 
+		$(document).ready(function(){
+		    $("#flip").click(function(){
+		        $("#panel").slideToggle("slow");
+		    });
+		});
+		</script>
+		
+		<style> 
+		#panel, #flip {
+		    padding: 15px;
+		    text-align: center;
+		    background-color: #f8f8f8;
+		    border: solid 1px #34c6be;
+		}
+		
+		#panel {
+		    padding: 50px;
+		    display: none;
+		}
+		</style>
 	</head>
 	<body>
 		<div class="div_root">
@@ -35,8 +57,39 @@
 		    <a href="#" class="btn btn-link">></a>
 		    </div>
 		  </li>
-			<!--java script 써서 추가해야할 것 같아요 -->
 		</ul>
+		<div id="flip">
+			<?php	
+				require_once('./php/config.php');
+			
+				$db = new DBC;	 //db object생성
+				$db->DBI();		//db 들어가기
+				
+				$db->query = "select * from notice";
+				$db->DBQ();
+				
+				while($data = $db->result->fetch_assoc())
+				{
+					echo $data['subject']. "(". $data['time']. ")". "<br>";
+				}
+			?>
+		</div>
+		<div id="panel">
+			<?php	
+				require_once('./php/config.php');
+			
+				$db = new DBC;	 //db object생성
+				$db->DBI();		//db 들어가기
+				
+				$db->query = "select * from notice";
+				$db->DBQ();
+				
+				while($data = $db->result->fetch_assoc())
+				{
+					echo $data['memo']. "<br>";
+				}
+			?>		
+		</div>
 		<hr/>		
 		</div>
 	</body>
