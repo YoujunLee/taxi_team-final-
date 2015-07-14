bc925b226fb6d4fc4b06cc48052f9b572290ce95
정마리아 <jmary625@google.com>
2015-07-14 15:38:18 +0900

    modify  프론트 page

--- index.php
+++ index.php
@@ -8,23 +8,19 @@ echo "<script>location.replace('./조회창.html.php');</script>";
 <html>
 	<head>
 		 <meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
    		 <title>i-Taxi</title>
    		<!-- // <meta name="viewport" content="width=device-width, initial-scale=1"> -->
    		 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
    		 <link rel="stylesheet" type="text/css" href="../css/로그인.css">	
-   		 <style>
-   		 	body{
-   		 		background-color: black;
-   		 	}
-   		 </style>
 	</head>
 	<body>
-		<div class = "col-xs-12  col-md-6 col-md-offset-3">
+		<div class = "col-xs-12  col-md-4 col-md-offset-4">
 		
   		<!-- <div class="div_root"> -->
 		<br><br><br><br>
 		<div align="center">
-			<img src="./img/logo_big.png">
+			<img width="80%" src="./img/logo_big.png">
 		</div>
 		<br><br><br>
   			<!-- Id 입력창 -->
@@ -46,20 +42,16 @@ echo "<script>location.replace('./조회창.html.php');</script>";
 				</span>
 				<input type="password" class="form-control" placeholder="Password" aria-describedby="sizing-addon1"  name="logpass" required>
 			</div>
-			<br><br>
-			<br><br><br>
+			<br><br><br><br>
 			<div class="div_go" class="main_color">
                 <input class="btn btn-lg btn-block" type="submit" value="GO">
 			</div>
 			
 		</form>
-			<br>
-				    <!-- 보기 좋게 띄우기 -->
-		    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+
+			<br>			
 		    <!-- 회원가입 버튼 -->
 		    <a href="./SignUp.html.php"><button class="btn btn-lg btn-block" class="main_color">회원가입</button></a>
-		    </div>
-		</div>
-		<!-- </div> -->
+		   </div>
 	</body>
 </html>
