87071a2b36db654ee20013ed20f2d0e20b2609ff
정마리아 <jmary625@google.com>
2015-07-17 13:53:54 +0900

    상단 글씨 없애기

--- 공지사항.html.php
+++ 공지사항.html.php
@@ -52,7 +52,6 @@
 			   <td class = "logo" >
 			      <a  href="./조회창.html.php"><img src="./img/logo.png"></a>
 			    </td >
-			     <td class = "title">공지사항</td >
 			      <td class = "logout">
 			      <form action='./php/logout.php'>
 					     <input class="btn1" type="submit" value="LogOut">
