de7f21ebb2a055f96cdf381425d405b3e5b008e2
p.k.KIM <kpk0314@naver.com>
2015-07-20 16:29:35 +0900

    천의 자리 입력시 콤마 설정

--- taxi_num.html.php
+++ taxi_num.html.php
@@ -29,16 +29,16 @@
 	<ul class="col-xs-12  col-md-4 col-md-offset-4 padding">
   <li><a href="tel:054-252-1111" class="eat"><span>
         <h1>포스콜</h1>
-        <p>here</p></span></a></li>
+        <p>054-252-1111</p></span></a></li>
   <li><a href="tel:054-283-8282" class="drink"><span>
         <h1>해맞이콜</h1>
-        <p>here</p></span></a></li>
+        <p>054-283-8282</p></span></a></li>
   <li><a href="tel:054-282-6161" class="play"><span>
         <h1>육일콜</h1>
-        <p>here</p></span></a></li>
+        <p>054-282-6161</p></span></a></li>
   <li><a href="tel:054-231-2330" class="relax"><span>
         <h1>영광콜</h1>
-        <p>here</p></span></a></li>
+        <p>054-231-2330</p></span></a></li>
 </ul>	
 
 </body>
