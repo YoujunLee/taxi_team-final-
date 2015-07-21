4255873e40d52430c78716c3cedaea88df1dab8b
정마리아 <jmary625@google.com>
2015-07-21 16:01:41 +0900

    finish QnA room

--- php/insert_QnA.php
+++ php/insert_QnA.php
@@ -7,6 +7,7 @@
 			include "./session_out.php";
 	        out();
 			require_once './config.php';
+			
 			$db = new DBC;
 			$db->DBI();
 			$db->query = "select num from QnA order by num desc limit 1";
@@ -16,13 +17,14 @@
 			$num1 = $db->result->num_rows;
 			$data = $db->result->fetch_row();
 			
-			
+			if( $db->result)
 			$num = $data[0]+1;
+			else
+			$num= 1;
 			
-			
-			$stu_id = $_POST['stu_id'];
-			$name = $_POST['name'];
-			$phone_num = $_POST['phone_num'];
+			$stu_id = $_SESSION['user_id'];
+			$name = $_SESSION['name'];
+			$phone_num = $_SESSION['cellphone'];
 			$subject = $_POST['subject'];
 			$memo= $_POST['memo'];
 			$time = date("Y-m-d");
@@ -33,7 +35,7 @@
 				exit;
 			}
 
-			$db->query = "insert into QnA values(".$num."','".$stu_id."','".$name."','".$phone_num."','" . $subject . "','".$memo."','".$time."')";
+			$db->query = "insert into QnA values('".$num."','".$stu_id."','".$name."','".$phone_num."','" . $subject . "','".$memo."','".$time."')";
 			$db->DBQ();
 			
 			if(!$db->result)
@@ -44,7 +46,7 @@
 			} 
 			else
 			{
-				echo "<script>location.replace('../QnA_room.html.php');</script>";
+				echo "<script>alert('문의가 등록되었습니다. 감사합니다.');history.back();</script>";
 			}
 		?>
 	</body>
