fc60ed49383c56b121db7cbe70412530c6ddf191
정마리아 <jmary625@google.com>
2015-07-21 14:45:39 +0900

    nothing special

--- update_mypage.html.php
+++ update_mypage.html.php
@@ -156,22 +156,26 @@
 		</form>
 	</div>
 		     <br/><br/><br/>
+		    
 		     <table class=" col-xs-12  col-md-4 col-md-offset-4">
 		     <tr>
 		     	<td>
-                <a href="./MyPage.html.php" class="btn btn-lg btn-block" style="background-color:red; color: #ffffff; opacity:0.8;" value="취소">취소</a>
+	                <a href="./MyPage.html.php" class="btn btn-lg btn-block" style="background-color:red; color: #ffffff; opacity:0.8;" value="취소">취소</a>
                 </td>
                 <td>
-                <input class="btn btn-lg btn-block" type="submit" style="background-color:#34C6BE; color: #ffffff;" value="변경">
+                	<input class="btn btn-lg btn-block" type="submit" style="background-color:#34C6BE; color: #ffffff;" value="변경">
 				</td>
 			</tr>
 			</table>
+			
+			
 		     <!--
 		     <div>
                 <a href="./MyPage.html.php" class="btn btn-lg btn-block" style="background-color:red; color: #ffffff; opacity:0.8;" value="취소">취소</a>
                 <input class="btn btn-lg btn-block" type="submit" style="background-color:#34C6BE; color: #ffffff;" value="변경">
 			</div>
 			-->
+
 	</body>
 </html>
 			
