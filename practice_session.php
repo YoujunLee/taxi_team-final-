e242ca45c500ebf517d1daefaaef9e3f459e7d72
yangmingyu <gt136@naver.com>
2015-07-22 11:40:11 +0900

    practice

--- practice_session.php
+++ practice_session.php
@@ -1,23 +1,34 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		 <meta charset="utf-8">
-		 </head>
-		 <body>
-		 
-<?php
- 
- $to = "gt136@naver.com";
- $subject = "Test";
- $message = "hello";
- $from = "gt136@naver.com";
- $header = "From:".$from."";
- $result = mail($to, $subject, $message,$header);
- if($result)
- echo "finish";
- else	
- echo "아놔";
- 
-?> 
-		 </body>
-		 </html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+
+<script type="text/javascript">
+
+function message(){
+	
+	var message = "당신은 남자입니까?";
+	
+	var result = confirm("취소");
+	
+	
+	if(result==true){
+	
+	alert("키큼?");
+	
+	}else{
+	
+	alert("예쁨?");
+	
+	}
+	
+}
+
+</script>
+</head>
+<body>
+
+<input type="button" value="confirm" onclick="message()">
+
+</body>
+</html>
