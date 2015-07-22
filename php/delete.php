8bc1358f6d9defb13acc9c7f8e2032b30fdbf4f4
yangmingyu <gt136@naver.com>
2015-07-22 14:37:42 +0900

    delete finish

--- php/delete.php
+++ php/delete.php
@@ -5,52 +5,21 @@
 <body>
 <?php
 	$hostname=$_SERVER["HTTP_REFERER"];
-	
+	$post_id=getenv("QUERY_STRING");
 	
 	echo "<script>
 		   var result=confirm('정말 안 타시겠습니까?');
 		   if(result==true){
-		  location.replace('http://www.naver.com/');
+		  location.replace('./delete2.php?".$post_id."');
 		   exit;
-		   }
+		   }else{
+		   			
+		   		history.go(-1);
+		   		exit
+		   	}
 		   </script>";
 		   	
-	include "./session_out.php";
-	out();
-	require_once './config.php';
 	
-	$post_id=getenv("QUERY_STRING");
-	$stu_id= $_SESSION['user_id'];
-	
-	/*탑승 취소시 room_user db에서 인원 삭제*/
-	$db = new DBC;
-	$db->DBI();//db 들어가기
-	$db->query = "DELETE FROM room_user WHERE post_id='".$post_id."' AND stu_id='".$stu_id."'";
-	$db->DBQ();
-	$db->DBO();
-	
-	/* 해당 Room에 몇명 남아 있는지 count*/
-	$db2 = new DBC;
-	$db2->DBI();
-	$db2->query= "SELECT post_id FROM room_user WHERE post_id='".$post_id."'";
-	$db2->DBQ();	
-	$num1 = $db2->result->num_rows;
-	$db2->DBO();
-	
-	/* 탑승인원 0명 일시 방 delete*/	
-	if($num1==0)
-	{
-		$db3 = new DBC;
-		$db3->DBI();//db 들어가기
-		$db3->query = "DELETE FROM post WHERE post_id='".$post_id."'";
-		$db3->DBQ();
-		$db3->query = "DELETE FROM comment WHERE post_id='".$post_id."'";
-		$db3->DBQ();
-		$db3->DBO();
-		
-		echo"<script>history.back();</script>";
-	}else{
-	echo"<script>history.back();</script>";}
 ?>
 </body>
 </html>
