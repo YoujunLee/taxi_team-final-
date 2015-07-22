a4d27739b9d9e06b2de470e81220fa8f4095be7a
정마리아 <jmary625@google.com>
2015-07-22 16:49:21 +0900

    탈퇴 완료!!!!!!!!!!!

--- php/withdrawal.php
+++ php/withdrawal.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		 <meta charset="utf-8">
-   		 <title>개인정보 수정</title>
+   		 <title>i-Taxi</title>
    		 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
    		 <link rel="stylesheet" type="text/css" href="../css/box.css">
 	</head>
@@ -14,45 +14,44 @@
 		
 		$db = new DBC; //db object생성
 		$db->DBI();//db 들어가기
-		include "./session_out.php";
-		out();
 		
 		$pass = $_POST['pass'];
+		session_start();
 		
 		$db->query = "select password from student_info where studentid = '".$_SESSION['user_id']."'";
 		$db->DBQ();
 		$data = $db->result->fetch_row();
-		$num = $db->result->num_rows;
 		
 		if(!$db->result)
 		{
-			echo "<script>alert('탈퇴에 실패하였습니다. <br/> 다시 시도해주세요.');history.back();</script>";
+			echo "<script>alert('탈퇴에 실패하였습니다. 다시 시도해주세요.');history.back();</script>";
 			$db->DBO();
 			exit;
 		}
 		
 		if (password_verify($pass, $data[0]))
 		{
-		  	
-			$db->query = "DELETE from student_info where studentid='".$_SESSION['user_id']."'";
+			$db->query = "delete from student_info where studentid='".$_SESSION['user_id']."'";
 			$db->DBQ();
 			
 			if(!$db->result)
 			{
 				
-				echo "<script>alert('탈퇴에 실패하였습니다. <br/> 다시 시도해주세요.');history.back();</script>";
+				echo "<script>alert('탈퇴에 실패하였습니다. 다시 시도해주세요.');history.back();</script>";
 				$db->DBO();
 				exit;
 				
 			}
 			else
 			{
-				echo "location.replace('../byebye.php');</script>";
+				echo "<script>location.replace('../byebye.html.php');</script>";
+				$db->DBO();
+				exit;
 			}
 		}  			// 비밀번호가 맞음 
 		else
 		{ 
-		     echo "<script>alert('비밀번호가 맞지 않습니다. <br/> 다시 입력해주세요.');history.back();</script>";
+		     echo "<script>alert('비밀번호가 맞지 않습니다. 다시 입력해주세요.');history.back();</script>";
 			$db->DBO();
 			exit;
 		}	        // 비밀번호가 틀림 
