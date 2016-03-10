734b68ab0c7f16366c212afb3f8b0b10f864f01e
YoujunLee <injxyj@gmail.com>
2016-03-10 15:46:51 +0900

    로그인 화면 민원 메일 변경

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ echo "<script>location.replace('./main.html.php');</script>";
 		
 			<div class="div2">
 			   <span>본 서비스는 모바일 환경에 최적화 되어 있습니다.</span> <br>
-			   <span>로그인 오류시 injxyj@gmail.com으로 메일주세요</span>
+			   <span>로그인 오류시 hguitaxi@gmail.com으로 메일주세요</span>
 			</div>
 		</div>
 	</body>
