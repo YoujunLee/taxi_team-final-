dfd804b6499fa7d3a029b496911ecbd2f6a8e34d
ymg <gt1136@naver.com>
2016-05-09 16:40:47 +0900

    agree.php 주석 완료

--- php/agree.php
+++ php/agree.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <?php
+//동의 두개 모두 했는지 체크 동의에 관한 데이터는 체크박스로서 post형식으로 넘어옴
 	if(!isset($_POST['agree'])||!isset($_POST['agree2']))
 		echo "<script>alert('모두 동의하셔야 다음 단계로 넘어갑니다.'); history.back();</script>";
 	
@@ -12,7 +13,7 @@
 	$agree2=$_POST['agree'];
 	
 	if($agree1!=null || $agree2!=null)
-		echo "<script>location.replace('../SignUp.html.php');</script>";
+		echo "<script>location.replace('../SignUp.html.php');</script>";//동의 두개 모두 했을 시 ../SignUp.html.php로 넘어가서 회원가입 진행
 ?>
 </body>
 </html>
