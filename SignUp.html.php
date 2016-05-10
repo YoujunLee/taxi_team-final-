48411cacd305d2f6b2531980677c9dcd789d3450
ymg <gt1136@naver.com>
2016-05-10 14:57:40 +0900

    SignUp.html.php주석완료

--- SignUp.html.php
+++ SignUp.html.php
@@ -34,12 +34,11 @@
 		</table>
 
 		<div class="form-group col-xs-12  col-md-4 col-md-offset-4">
-<<<<<<< HEAD
+
 		<table class="table">
-=======
-			<table class="table">
->>>>>>> b2be1ba44f8a24f2f0e520433f672efc0d2b6c4c
+
 			<thead>
+			<!-- 메일주소 -->
 			<tr>
 					<td colspan=2>
 						<h2 style="color:#34C6BE">WELCOME!</h2>
@@ -49,8 +48,9 @@
 			</thead>	
 			<tbody>
 				<tr>
-				<form action='./php/registo.php' method='POST'>
+				<form action='./php/registo.php' method='POST'> <!-- 모든 정보 기입시에 registo.php로 post형태로 데이터 넘어감	 -->
 					<label for="inputName">
+					<!-- 이름 -->
 					<td class="col-md-3">
 						<b>이름</b>
 					</td>
@@ -62,11 +62,12 @@
 				
 				<tr>
 					<label for="inputStudentId">
+						<!-- 학번 -->
 						<td>
 							<b>학번</b>
 						</td>
 						<td>
-							 <input type="tel"  onKeyPress="if ((event.keyCode<46)||(event.keyCode>57)||(event.keyCode==47)) event.returnValue=false;" class="form-control" id="inputStudentId" placeholder="Student ID" name='student_no' maxlength="8" required>
+							 <input type="tel"  onKeyPress="if ((event.keyCode<46)||(event.keyCode>57)||(event.keyCode==47)) event.returnValue=false;" class="form-control" id="inputStudentId" placeholder="Student ID" name='student_no' maxlength="8" required><!-- 학번 특성상 숫자만 쓸 수 있고 8자리까지 쓸 수 있게 만듬 -->
 						</td>
 					</label>
 				</tr>
@@ -82,9 +83,9 @@
 		      				<script>
 		      					function autoHypenPhone(str)
 								{
-									str = str.replace(/[^0-9]/g, '');
+									str = str.replace(/[^0-9]/g, '');<!-- 숫자 외의 문자가 들어올경우 없앰 -->
 									var tmp = '';
-	
+									<!-- 전화번호의 경우 숫자 중간중간에 - 가 들어가도록 코드를 짬 -->
 									if( str.length < 4)
 										return str;  
 								    else if(str.length < 7){
@@ -112,18 +113,18 @@
 									return str;
 								}
 								
-								var Phone = document.getElementById('Phone');
+								var Phone = document.getElementById('Phone'); <!-- 전화번호 받는 input 태그의 아이디를 Phone으로 지정해서 사용자가 쓴 문자를 받아옴 -->
 								     			
 								Phone.onkeyup = function(event){
 								   	event = event || window.event;
 								   	var _val = this.value.trim();
-								 	this.value = autoHypenPhone(_val) ;
+								 	this.value = autoHypenPhone(_val) ;<!-- 전화번호 입력란에 문자를 쓸 때 마다 작동 -->
 								}
 		      				</script>
 						</td>
 					</label>
 				</tr>
-				
+				<!-- 비밀번호 -->
 				<tr>
 					<label for="inputPassword">
 						<td>
@@ -149,6 +150,7 @@
 				
 		    			  <!-- 비밀번호 확인-->
 		    			  <script>
+							  //비밀번호를 입력란에 쓴 비밀번호와 비밀번호를 재확인하는 입력란에 쓴 비밀번호가 일치하는지 체크한다.
 		      			  	var pass1 = document.getElementById('inputPassword');
 		      				var pass2 = document.getElementById('inputPasswordTwice');
 		     				var test = document.getElementById('test');
@@ -169,7 +171,7 @@
                				}
              				}
                				pass2.onkeyup = function(event){
-
+							//비밀번호가 일치하는지 불일치 하는지 비밀번호 입력란에 문자를 쓸 때마다 텍스트가 뜨게끔 구현
                				event = event || window.event;
                 			if(pass2.value == null || pass2.value == ""){
                 				test.value ="";
@@ -186,6 +188,7 @@
 				
 				<tr>
 					<label>
+						<!-- 비밀번호 찾는 질문 과 답 -->
 						<td>
 							<b>비밀번호 찾을시 <br> 질문</b>
 						</td>
