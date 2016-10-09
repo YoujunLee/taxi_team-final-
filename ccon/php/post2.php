84570b9fccb3eac8ece012e98bb9b3fbed0ea7d3
YoujunLee <injxyj@gmail.com>
2016-10-09 22:39:55 +0900

    오류 수정

--- ccon/php/post2.php
+++ ccon/php/post2.php
@@ -6,34 +6,37 @@ $link1 = mysqli_connect('211.108.60.110', 'cconmausa_dev', '#zoq6ton', 'cconma_D
 $api_url = 'https://a68a232e3c3e4b669d1d3f9e91e0d403:ed2915c40d19c72aeb0b97dc5439d1e8@cconmausa.myshopify.com';
 $count = 0; //for the test
 
+$prod_obj_url = $api_url . '/admin/products.json';
+$prod_count_url = $api_url.'/admin/products/count.json';
+
 /*CCONMA DB에서 추출해서 products.json으로 migration*/
 if($result = mysqli_query($link, 'SELECT * FROM ez_product', MYSQLI_USE_RESULT)){
     while($row = mysqli_fetch_object($result)){
 
         if($count>15)
-           break;
+            break;
 
         $count++;
 
         /*모든 문자값들은 euc-kr에서 UTF-8로 변환 후 migration 진행*/
         $title = mb_convert_encoding($row->prdname, "UTF-8", "euc-kr");
         $company = mb_convert_encoding($row->prdcom, "UTF-8", "euc-kr");
-
+        echo $company;
         $product=array('title' => $title,
-        'body_html' => 'default',
-        'vendor' => $company,
-        'product_type' => $row->ptype,
-        "images"=>array( array ("src"=>'http://www.cconmausa.com/prdimg/'.$row->prdcode.'_M1.jpg')),
-        'variants' => array(
-            array('option2' => 'Defau',
-                'price' => $row->sellprice,
-                'sku' => $row->prdcode,
-                'inventory_quantity' => $row->stock,
-                'inventory_management' => 'shopify',
-                'taxable' => true,
-                'requires_shipping' => true
-            )
-        ));
+            'body_html' => 'default',
+            'vendor' => $company,
+            'product_type' => $row->ptype,
+            //"images"=>array( array ("src"=>'http://www.cconmausa.com/prdimg/'.$row->prdcode.'_M1.jpg')),
+            'variants' => array(
+                array('option2' => 'Defau',
+                    'price' => $row->sellprice,
+                    'sku' => $row->prdcode,
+                    'inventory_quantity' => $row->stock,
+                    'inventory_management' => 'shopify',
+                    'taxable' => true,
+                    'requires_shipping' => true
+                )
+            ));
 
         $ch = curl_init($api_url.'/admin/products.json');
         $data_string = json_encode(array('product'=>$product));
@@ -41,19 +44,21 @@ if($result = mysqli_query($link, 'SELECT * FROM ez_product', MYSQLI_USE_RESULT))
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'ContentLength: ' . strlen($data_string))
+                'Content-Type: application/json',
+                'ContentLength: ' . strlen($data_string))
         );
 
         /*http response를 받아 그 중 shopify용 id값을 받는다.*/
-        $http_response = curl_exec ($ch);
-        print_r($http_response);
-        $response = json_decode($http_response, true);
-        $response_id = $response['product'];
-        $id= $response_id['id'];
+        //$http_response = curl_exec ($ch);
+        //print_r($http_response);
+        //$response = json_decode($http_response, true);
+        //$response_id = $response['product'];
+        //$id= $response_id['id'];
+        //$cur_date =  date("Y-m-d H:i:s",time());
+      //  echo $cur_date;
 
         /*DB에 저장한다 ($id = shopify id / $row->prdcode = cconma id)*/
-        mysqli_query($link1, "insert into ez_shopify_id values ('".$id."','".$row->prdcode."')", MYSQLI_USE_RESULT);
+        //mysqli_query($link, "insert into ez_shopify_id values ('".$id."','".$row->prdcode."')", MYSQLI_USE_RESULT);
     }
 }
 
