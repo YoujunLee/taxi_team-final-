b936b788cc3b6937a4402674981c7f31e88d92d4
YOUJUN LEE <youjunlee@YOUJUNui-MacBook-Pro.local>
2017-05-12 11:39:56 +0900

    참여중 표시 완성

--- api/make_room.php
+++ api/make_room.php
@@ -1,3 +1,4 @@
+<!--동일시간 같은방 예외처리-->
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json;charset=UTF-8");
@@ -23,7 +24,9 @@ $request = json_decode($postdata);
 $username=$request->start;
 echo json_encode("Username is ".$username);
 
-$stu_id=21101002;
+$stu_id=$request->stu_id;
+$cellphone=$request->cellphone;
+$name=$request->name;
 $room_start=$request->start;
 $room_arrive=$request->arrive;
 $room_date=$request->date;
@@ -37,5 +40,26 @@ $db2->query = "insert into post values ('".$post_id ."','".$stu_id."','".$room_s
 $db2->DBQ();
 $db2->DBO();
 
+$db3 = new DBC; //db object생성
+$db3->DBI();//db 들어가기
+$db3->query = "SELECT id FROM room_user ORDER BY id desc LIMIT 1";
+$db3->DBQ();
+$num1 = $db3->result->num_rows;
+$data1 = $db3->result->fetch_row();
+if($num1==1)
+{
+	$id = $data1[0]+1;	
+}
+else
+{
+	$id=1;
+}
+$db4=new DBC;
+$db4->DBI();
+$db4->query = "insert into room_user values('".$id."', '".$post_id ."','".$stu_id."','".$name."','".$cellphone."','".$room_start."', '".$room_arrive."','".$room_date."','".$room_time."')";
+$db4->DBQ();
+
+$db3->DBO();
+$db4->DBO();
 /*방 참여하기*/
 ?>
--- api/used.php
+++ api/used.php
@@ -3,38 +3,22 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json;charset=UTF-8");
 require_once '../php/config.php';
-date_default_timezone_set("Asia/Seoul");
-$current_time2 = date("Y-m-d");
-$current_time3 = date("H:i:s");
 
-$user_id=getenv("QUERY_STRING"); // Get값으로 넘어온 값들을 구합니다.
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$user_id=$request->stu_id;
+$post_id=$request->post_id;
+$check=0;
 
 $db3 = new DBC;
 $db3->DBI();
-$db3->query= "SELECT post_id FROM room_user WHERE stu_id='".$user_id."' ORDER BY date desc,time desc";
+$db3->query= "SELECT stu_id FROM room_user WHERE post_id='".$post_id."'";
 $db3->DBQ();
 				
-$db = new DBC;
-$db->DBI();
-
-$result=array();
-
-while($data= $db3->result->fetch_row())
-{
-	$db->query = "SELECT start, arrive, date, time, population, post_id FROM post WHERE post_id='".$data[0]."' ";
-	$db->DBQ();
-
-    while($data2 = $db->result->fetch_array())
-    {
-        $data2_array['post_id']=$data2['post_id'];
-        $data2_array['start']=$data2['start'];
-        $data2_array['arrive']=$data2['arrive'];
-        $data2_array['date']=$data2['date'];
-        $data2_array['time']=$data2['time'];
-        $data2_array['population']=$data2['population'];
-        array_push($result, $data2_array);
-    }
+while($data = $db3->result->fetch_assoc()){
+    if($data['stu_id']==$user_id)
+     $check=1;
 }
-
-echo json_encode ($result, JSON_UNESCAPED_UNICODE);
+echo json_encode($check);
 ?>
